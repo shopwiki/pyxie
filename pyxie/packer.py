02e32bf4b2aa6f7fa82b9558b3907fc60952f9be
Jason Moiron <jmoiron@jmoiron.net>
2010-04-13 11:31:25 -0400

    adding lots of packer stuff, fairly comprehensive tests for pyxie

--- pyxie/packer.py
+++ pyxie/packer.py
@@ -10,6 +10,8 @@ http://stackoverflow.com/questions/1213394/algorithm-needed-for-packing-\
 Start with the largest rectangle.  Add the next largest rectangle to the place
 that extends the pack region as little as possible.  Repeat until you have
 placed the smallest rectangle.
+
+The coordinate system used here has the origin (0, 0) at the top-left.
 """
 
 class Rectangle(object):
@@ -17,25 +19,63 @@ class Rectangle(object):
         self.x, self.y = x, y
         self.data = data
 
+    def __repr__(self):
+        return '<Rectangle %d, %d>' % (self.x, self.y)
+
 class Point(object):
     def __init__(self, x, y):
         self.x, self.y = x, y
 
-class Field(object):
-    class PositionedRectangle(object):
-        def __init__(self, x, y, rect):
-            self.x, self.y, self.rect = x, y, rect
-            self.bl, self.tr = None, None
+class Line(object):
+    """A very simplistic grid line class, where all lines are either vertical
+    or horizontal."""
+    def __init__(self, p1, p2):
+        """Make sure that p1.x is the left-most top-most point in the line."""
+        if p1.x == p2.x: # vertical
+            self.vertical = True
+            if p1.y < p2.y: self.p1, self.p2 = p1, p2
+            else: self.p1, self.p2 = p2, p1
+        elif p1.y == p2.y: # horizontal
+            self.vertical = False
+            if p1.x < p2.x: self.p1, self.p2 = p1, p2
+            else: self.p1, self.p2 = p2, p1
+        else:
+            raise Exception("Line objects can only have horizontal or vertical lines.")
 
-        def contains_point(self, point):
-            """This positioned rectangle contains point (x,y) if x is between
-            the left-most x and the right-most x, and y is between the top-most
-            y and bottoom-most y."""
-            if (point.x > self.x) and (point.x < (self.x + self.rect.x)) and\
-               (point.y > self.y) and (point.y < (self.y + self.rect.y)):
-               return True
+    def __contains__(self, p):
+        """Return whether or not this line contains a point p."""
+        if self.vertical: # vertical line
+            if p.x == self.p1.x and p.y >= self.p1.y and p.y <= self.p2.y:
+                return True
+            return False
+        else:
+            if p.y == self.p1.y and p.x >= self.p1.x and p.x <= self.p2.x:
+                return True
             return False
 
+
+class PositionedRectangle(object):
+    """A rectangle positioned within a field.  Has the coordinates of the
+    rectangle and whether or not there's another rectangle positioned at
+    its top-right or bottom-left corner."""
+    def __init__(self, x, y, rect):
+        self.x, self.y, self.rect = x, y, rect
+        self.bl, self.tr = None, None
+
+    def __contains__(self, point):
+        """This positioned rectangle contains point (x,y) if x is between
+        the left-most x and the right-most x, and y is between the top-most
+        y and bottoom-most y."""
+        if (point.x > self.x) and (point.x < (self.x + self.rect.x)) and\
+           (point.y > self.y) and (point.y < (self.y + self.rect.y)):
+           return True
+        return False
+
+    def __repr__(self):
+        return '<pRect @ (%d, %d), %dx%d (tr/bl: %r, %r)>' % (self.x, self.y,
+                self.rect.x, self.rect.y, self.tr, self.bl)
+
+class Field(object):
     def __init__(self, x=0, y=0):
         self.x, self.y = x, y
         self.rectangles = []
@@ -53,28 +93,32 @@ class Field(object):
         intensive, but we should never be dealing with a great deal of images."""
         if not self.rectangles:
             self.rectangles.append(PositionedRectangle(0, 0, rectangle))
+            self.x, self.y = self.calculate_bounds()
+            return
         attempts = []
         for rect in self.rectangles:
             for placement in (self.bottom_left, self.top_right):
                 result = placement(rect, rectangle)
-                if res is 0:
+                if result is 0:
                     placement(rect, rectangle, place=True)
                     return
                 # if we didn't have a collision
-                if res is not None:
+                if result is not None:
                     attempts.append((result, placement, rect))
         attempts.sort()
         result, placement, rect = attempts[0]
-        print "Area increasing from %d to %d" % (self.area(), result)
+        #print "Area increasing from %d to %d" % (self.area(), result)
         placement(rect, rectangle, place=True)
+        self.x, self.y = self.calculate_bounds()
 
     def bottom_left(self, placed, new, place=False):
         """Attempt to place a new rectangle on the bottom left corner of a
         previously placed rectangle.  Return the amt that the overall area of
         the field would increase, or None if a collision is detected."""
         if place:
+            self.mark_corners(placed.x, placed.y + placed.rect.y, new)
             self.rectangles.append(PositionedRectangle(placed.x, placed.y + placed.rect.y, new))
-            self.x, self.y = self.calculate_area()
+            self.x, self.y = self.calculate_bounds()
             return
         if placed.bl:
             return None
@@ -86,8 +130,9 @@ class Field(object):
 
     def top_right(self, placed, new, place=False):
         if place:
+            self.mark_corners(placed.x, placed.y + placed.rect.y, new)
             self.rectangles.append(PositionedRectangle(placed.x + placed.rect.x, placed.y, new))
-            self.x, self.y = self.calculate_area()
+            self.x, self.y = self.calculate_bounds()
             return
         if placed.tr:
             return None
@@ -95,28 +140,57 @@ class Field(object):
         if not self.collision(corner, new):
             return self.new_area(corner, new)
 
+    def mark_corners(self, x, y, rect):
+        """Find all of the rectangles whose top-right or bottom-left corner are
+        "occupied" by the new rectangle, and mark them appropriately."""
+        left = Line(Point(x, y), Point(x, y + rect.y))
+        top = Line(Point(x, y), Point(x + rect.x, y))
+        # for every rectangle, if the top right or bottom left corners are in
+        # these lines, mark them as blocked
+        for pos in self.rectangles:
+            if not pos.tr:
+                if Point(pos.x + pos.rect.x, pos.y) in left:
+                    pos.tr = True
+            if not pos.bl:
+                if Point(pos.x, pos.y + pos.rect.y) in top:
+                    pos.bl = True
+        return True
+
+
     def new_area(self, corner, new):
         """Return the new area of the field given a rectangle is positioned
         with its top left corner at `corner`."""
-        pass
+        if isinstance(corner, tuple):
+            corner = Point(*corner)
+        x, y = self.calculate_bounds(self.rectangles + [PositionedRectangle(corner.x, corner.y, new)])
+        return x * y
 
     def calculate_bounds(self, rectangles=None):
         """Calculate x/y bounds for a field with the given rectangles.  If
         rectangles is None, calculate it for self's rectangles."""
         if rectangles is None:
             rectangles = self.rectangles
-        return None
+        def span(rectangles):
+            # only rectangles without another positioned in the top right
+            possibilities = [r for r in rectangles if not r.tr]
+            return max([(r.x + r.rect.x) for r in possibilities])
+        def range(rectangles):
+            # only rectangles without another positioned in the bottom left
+            possibilities = [r for r in rectangles if not r.bl]
+            return max([(r.y + r.rect.y) for r in possibilities])
+        return span(rectangles), range(rectangles)
 
     def collision(self, corner, new):
         def collide(rect, corner, new):
+            tl = Point(*corner)
             points = [
-                Point(*corner),
-                Point(tl.x + new.x, tl.y),
-                Point(tl.x, new.y + tl.y),
-                Point(tl.x + new.x, tl.y + new.y),
+                tl, # top left
+                Point(tl.x + new.x, tl.y), # top right
+                Point(tl.x, new.y + tl.y), # bottom left
+                Point(tl.x + new.x, tl.y + new.y), # bottom right
             ]
             for point in points:
-                if rect.contains_point(point):
+                if point in rect:
                     return True
             return False
         for rect in self.rectangles:
---
+++ tests/pyxietest.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""pyxie tests."""
+
+from unittest import TestCase
+from pyxie import packer
+
+class LineTest(TestCase):
+    def test_line_contains(self):
+        """Test the line class & intersection detection."""
+        Line, Point = packer.Line, packer.Point
+        l1 = Line(Point(10, 10), Point(10, 50))
+        l2 = Line(Point(10, 5), Point(32, 5))
+
+        self.failUnless(l1.vertical)
+        self.failUnless(not l2.vertical)
+
+        # test middle intersections
+        self.failUnless(Point(10, 30) in l1)
+        self.failUnless(Point(25, 5) in l2)
+
+        # test endpoint intersections
+        self.failUnless(Point(10, 10) in l1)
+        self.failUnless(Point(10, 50) in l1)
+        self.failUnless(Point(10, 5) in l2)
+        self.failUnless(Point(32, 5) in l2)
+
+        # test non intersecting points at beginning & end of lines
+        self.failUnless(Point(10, 9) not in l1)
+        self.failUnless(Point(10, 51) not in l1)
+        self.failUnless(Point(9, 5) not in l2)
+        self.failUnless(Point(33, 5) not in l2)
+
+        # test that weird lines are exceptions
+        def skewed(p1, p2):
+            return Line(p1, p2)
+
+        self.assertRaises(Exception, skewed, (Point(10, 10), Point(20, 20)))
+        self.assertRaises(Exception, skewed, (Point(10, 10), Point(10, 10)))
+
+
+class PackerTest(TestCase):
+
+    def test_packer_placement(self):
+        # if these are from largest to smallest, we should mostly know the result
+        rects = [packer.Rectangle(*args) for args in [
+            (128, 64),
+            (128, 32),
+            (64, 64),
+            (128, 16),
+            (32, 32),
+        ]]
+        f = packer.Field()
+
+        # add 128x64 to the top left:
+        # +-------------+
+        # |             | 128x64
+        # +-------------+
+        #
+        f.add_rectangle(rects[0]) # 128, 64
+        self.failUnless(f.area() == 8192)
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 64)
+
+        # add 128x32 to bottom left to make a 128x96 field:
+        # +-------------+
+        # |             | 128x64
+        # +-------------+
+        # +-------------+ 128x32
+        #
+        f.add_rectangle(rects[1]) # 128, 32
+        self.failUnless(f.area() == 12288)
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 96)
+
+        # add 64x64 to top-right of 128x64 to increase to 192x96, < 128x160
+        # +-------------+-------+
+        # |  128x64     | 64x64 |
+        # +-------------+-------+
+        # +-------------+ 128x32
+        #
+        f.add_rectangle(rects[2]) # 64, 64
+        self.failUnless(f.area() == (192*96))
+        self.failUnless(f.x == 192)
+        self.failUnless(f.y == 96)
+
+        # add 128x16 to bottom-left of 128x32 to increase to 192x112, < 256x96
+        # +-------------+-------+
+        # |  128x64     | 64x64 |
+        # +-------------+-------+
+        # +-------------+ 128x32
+        # +-------------+ 128x16
+        #
+        f.add_rectangle(rects[3]) # 128, 16
+        self.failUnless(f.area() == (192*112))
+        self.failUnless(f.x == 192)
+        self.failUnless(f.y == 112)
+
+        # finally, add 32x32 to the top-right of 128x32 (or bottom-left of 64x64)
+        # +-------------+-------+
+        # |  128x64     | 64x64 |
+        # +-------------+---+---+
+        # +-------------+---+ 32x32
+        # +-------------+ 128x16
+        #    128x32
+        f.add_rectangle(rects[4]) # 32, 32
+        self.failUnless(f.area() == (192*112))
+        self.failUnless(f.x == 192)
+        self.failUnless(f.y == 112)
+        self.failUnless(len(f.rectangles) == len(rects))
+
+    def test_vertical_run_detection(self):
+        """Test the packer's ability to detect vertical runs;  when the addition
+        of a rectangle to the top-right of one rectangle's corner actually blocks
+        more than one rectangle's top-right corner."""
+        rects = [packer.Rectangle(*args) for args in [
+            (128, 64),
+            (128, 32),
+            (64, 96),
+        ]]
+        f = packer.Field()
+
+        # add 128x64 to the top left:
+        # +-------------+
+        # |             | 128x64
+        # +-------------+
+        #
+        f.add_rectangle(rects[0]) # 128, 64
+        self.failUnless(f.area() == 8192)
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 64)
+
+        # add 128x32 to bottom left to make a 128x96 field:
+        # +-------------+
+        # |             | 128x64
+        # +-------------+
+        # +-------------+ 128x32
+        #
+        f.add_rectangle(rects[1]) # 128, 32
+        self.failUnless(f.area() == 12288)
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 96)
+
+        # add 64x64 to top-right of 128x64 to increase to 192x96, < 128x160
+        # +-------------+-------+
+        # |  128x64     | 64x96 |
+        # +-------------+       |
+        # +-------------+-------+
+        #    128x32
+        f.add_rectangle(rects[2]) # 64, 96
+        self.failUnless(f.area() == (192*96))
+        self.failUnless(f.x == 192)
+        self.failUnless(f.y == 96)
+
+        # make sure that only 2 rectangles have tr set, and that it is
+        # the first two we've added.
+        has_top_right = [r for r in f.rectangles if r.tr]
+        self.failUnless(len(has_top_right) == 2)
+        self.failUnless(f.rectangles[0].tr)
+        self.failUnless(f.rectangles[1].tr)
+
+    def test_horizontal_run_detection(self):
+        """Test the packer's ability to detect horizontal runs;  when the
+        addition of a rectangle to the bottom-left of one rectangle's corner
+        blocks more than one rectangle's bottom-left corner."""
+        rects = [packer.Rectangle(*args) for args in [
+            (128, 64),
+            (64, 16),
+            (32, 16),
+            (32, 16),
+            (128, 2),
+        ]]
+        f = packer.Field()
+
+        # add 128x64 to the top left:
+        # +-------------+
+        # |             | 128x64
+        # +-------------+
+        #
+        f.add_rectangle(rects[0]) # 128, 64
+        self.failUnless(f.area() == 8192)
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 64)
+
+        # add 64x16 to bottom left to make a 128x80 field:
+        # +-------------+
+        # |             | 128x64
+        # +-----+-------+
+        # +-----+ 64x16
+        #
+        f.add_rectangle(rects[1]) # 64, 16
+        self.failUnless(f.area() == (128 * 80))
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 80)
+
+        # add two 32x16 to fill out the bottom of 128x64
+        # +-------------+
+        # |             | 128x64
+        # +-----+---+---+
+        # +-----+---+---+ 64x16, 32x16, 32x16
+        #
+        f.add_rectangle(rects[2]) # 32, 32
+        self.failUnless(f.area() == (128 * 80))
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 80)
+
+        f.add_rectangle(rects[3]) # 32, 32
+        self.failUnless(f.area() == (128 * 80))
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 80)
+
+        # add the long 128x2 rectangle, which should go to the bottom-left of
+        # the 64x64 rectangle and take up the bottom-left of rects 2,3 and 4
+        # +-------------+
+        # |             | 128x64
+        # +-----+---+---+ 
+        # +-----+---+---+ 64x16, 32x16, 32x16
+        # +-------------+ 128x2
+        f.add_rectangle(rects[4]) # 128, 2
+        self.failUnless(f.area() == (128 * 82))
+        self.failUnless(f.x == 128)
+        self.failUnless(f.y == 82)
+
+        has_bottom_left = [r for r in f.rectangles if r.bl]
+        self.failUnless(len(has_bottom_left) == 1)
+        self.failUnless(f.rectangles[1].bl)
+        self.failUnless(f.rectangles[2].bl)
+        self.failUnless(f.rectangles[3].bl)
+
